624d922ab879f968ed71142f41c4fabb2f900781
shilpashingnapure <shingnapureshilpa2@gmail.com>
2024-06-03 00:01:45 +0530

    added

--- calendar-system-backend/src/config/db.js
+++ calendar-system-backend/src/config/db.js
@@ -2,8 +2,11 @@ import mongoose from 'mongoose';
 
 export const dbConnection = async () => {
     try {
-        mongoose.connect('mongodb://localhost:27017/db')
+        mongoose.connect('mongodb+srv://shingnapureshilpa2:Gold@123@cluster0.zeevvvd.mongodb.net/db')
+
     }catch (err){
         console.log(err);
     }
 }
+
+
