47f2820a97a5121cefdbbd8bb72acdc17651ee16
shilpashingnapure <shingnapureshilpa2@gmail.com>
2024-06-03 01:12:49 +0530

    slogs

--- calendar-system-backend/src/index.js
+++ calendar-system-backend/src/index.js
@@ -11,26 +11,31 @@ import { updateEvent } from "./controllers/update-event.js";
 import { deleteEvent } from "./controllers/delete-event.js";
 import { getAllEvents } from "./controllers/all-events.js";
 
-
 // middlewares
 dotenv.config({});
 const app = express();
 app.use(cors());
 
 // preflight req
-app.options('*', cors());
+app.options("*", cors());
 app.use(express.json());
-
 
 // user auth login
 app.post("/auth/google", async (req, res) => {
   try {
     const { code } = req.body;
-    console.log('code' , code);
-    console.log('client' , client);
+    console.log("code", code);
+    console.log(
+      "client",
+      client,
+      process.env.SECRET_KEY,
+      process.env.CLIENT_ID,
+      process.env.CLIENT_SECRET,
+      process.env.REDIRECT_URL
+    );
     const { tokens } = await client.getToken(code);
 
-    console.log('tokens' ,  tokens);
+    console.log("tokens", tokens);
 
     // verity token
     const tokenResponse = await client.verifyIdToken({
@@ -60,7 +65,10 @@ app.post("/auth/google", async (req, res) => {
     await user.save();
 
     // generate JWT token
-    const userToken = jwt.sign({ googleId, userid: user._id }, process.env.SECRET_KEY);
+    const userToken = jwt.sign(
+      { googleId, userid: user._id },
+      process.env.SECRET_KEY
+    );
     res.status(200).send({
       message: "Login successful",
       token: userToken,
@@ -72,16 +80,14 @@ app.post("/auth/google", async (req, res) => {
   }
 });
 
-
-// event CURD oprations 
-app.get("/events", authenticateToken , getAllEvents);
+// event CURD oprations
+app.get("/events", authenticateToken, getAllEvents);
 
 app.post("/create-event", authenticateToken, createEvent);
 
-app.patch("/update-event/:id", authenticateToken , updateEvent);
+app.patch("/update-event/:id", authenticateToken, updateEvent);
 
-app.delete("/delete-event/:id" , authenticateToken , deleteEvent)
-
+app.delete("/delete-event/:id", authenticateToken, deleteEvent);
 
 const port = 8000;
 app.listen(port, () => {
